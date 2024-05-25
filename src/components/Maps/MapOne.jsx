82ed990a7dd5820b3797ba13bc66f63b21675a4a
ramps <tech@billsakshi.com>
2024-05-26 03:05:29 +0530

    first

---
+++ src/components/Maps/MapOne.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MapOne = () => {
+  return (
+    <div>MapOne</div>
+  )
+}
+
+export default MapOne
---
+++ src/css/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import img1 from "../images/icon/i"
+// import img1 from "./images/icon/icon-calendar.svg"
+
+const Test = () => {
+  return (
+    <div>Test</div>
+  )
+}
+
+export default Test
